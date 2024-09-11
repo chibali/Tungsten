41c3cd1fb8f1fc9905d4f3dcac4a644950d41c5c
chibali <diogomnuness@hotmail.com>
2024-09-11 17:20:25 +0100

    Configured EnhancedInputComponents and Mapping tags for actions. Added move and look around inputs

---
+++ Source/Tungsten/Public/Components/Input/TungstenInputComponent.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "DataAssets/Input/DataAsset_InputConfig.h"
+#include "TungstenInputComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TUNGSTEN_API UTungstenInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+	
+public:
+	template<class UserObject, typename CallbackFunc>
+	void BindNativeInputAction(const UDataAsset_InputConfig* InInputConfig, const FGameplayTag& InInputTag, ETriggerEvent TriggerEvent, UserObject* ContextObject, CallbackFunc Func);
+};
+
+template<class UserObject, typename CallbackFunc>
+inline void UTungstenInputComponent::BindNativeInputAction(const UDataAsset_InputConfig* InInputConfig, const FGameplayTag& InInputTag, ETriggerEvent TriggerEvent, UserObject* ContextObject, CallbackFunc Func)
+{
+	checkf(InInputConfig, TEXT("Input config data asset is null and cannot proceed with binding"));
+	if (UInputAction* FoundAction = InInputConfig->FindNativeInputActionByTag(InInputTag))
+	{
+		BindAction(FoundAction, TriggerEvent, ContextObject, Func);
+	}
+
+}
